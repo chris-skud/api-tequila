18e61f61d243f4edcf273cd30b9e8b0c9aa6ed29
Chris Skudlarczyk <chris.skud@gmail.com>
2014-12-06 23:00:15 -0700

    Initial brands request spec.

---
+++ spec/requests/brands_request_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+require 'json_request_helper'
+
+RSpec.describe 'Brands' do
+
+  context 'when #index requested' do
+    let(:brands) {json[:brands]}
+
+    it 'returns producers collection' do
+      brand = FactoryGirl.create(:brand)
+
+      get '/brands'
+
+      it_responds_with_code_200
+      expect(brands.first[:name]).to eq brand.name
+    end
+  end
+end
